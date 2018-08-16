c6a9eb6393605278822240911d248bbdf8f525f5
Kirstin Ryan <ryan.kirstin@gmail.com>
2018-08-16 12:24:05 +0100

    DBHelper CRUD actions tried tested and passed for basic administrator sub class

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,30 @@
+import db.DBHelper;
+import models.Administrator;
+
+import java.util.List;
+
+import static db.DBHelper.getAll;
+
 public class Runner {
 
     public static void main(String[] args) {
+
+        Administrator joe = new Administrator("Joe", "PS303576D", 20000);
+        DBHelper.save(joe);
+
+        Administrator mike = new Administrator("Mike", "PS356576D", 50000);
+        DBHelper.save(mike);
+        mike.setSalary(60000);
+        DBHelper.update(mike);
+
+        Administrator emil = new Administrator("Emil", "PS303766D", 100000);
+        DBHelper.save(emil);
+        DBHelper.delete(emil);
+
+        List<Administrator> administrators = DBHelper.getAll(Administrator.class);
+
+        Administrator findJoe = DBHelper.findbyId(Administrator.class, joe.getId());
+
 
     }
 
--- src/main/java/models/Administrator.java
+++ src/main/java/models/Administrator.java
@@ -8,6 +8,9 @@ import javax.persistence.*;
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Administrator extends Employee {
 
+//    Create a Administrator class which inherits from the employee class. The administrator should also have a manager.
+
+
     public Administrator(String name, String niNumber, int salary) {
         super(name, niNumber, salary);
     }
